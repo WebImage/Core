76c167e8e1733a1bec84dea15a6ba7b5648f6705
Robert Jones II <rjones@corporatewebimage.com>
2019-08-22 09:06:19 -0400

    Added AbstractServiceProvider for added functionality

---
+++ src/Container/ServiceProvider/AbstractServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WebImage\Container\ServiceProvider;
+
+use WebImage\Application\ApplicationInterface;
+use WebImage\Config\Config;
+
+abstract class AbstractServiceProvider extends \League\Container\ServiceProvider\AbstractServiceProvider
+{
+	/**
+	 * Convenience method for returning application configuration
+	 * @return Config
+	 */
+	public function getApplicationConfig(): Config
+	{
+		return $this->getApplication()->getConfig();
+	}
+	/**
+	 * Convenience method for returning application instance
+	 * @return ApplicationInterface
+	 */
+	public function getApplication(): ApplicationInterface
+	{
+		return $this->getContainer()->get(ApplicationInterface::class);
+	}
+}
