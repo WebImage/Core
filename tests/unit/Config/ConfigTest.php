f8e10a557f51aa0b126f3af3018df3afcf36e6b8
Robert Jones II <rjones@corporatewebimage.com>
2024-09-06 10:37:15 -0700

    Added ConfigTest

---
+++ tests/unit/Config/ConfigTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace WebImage\Config;
+
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+	public function testGetTraversal()
+	{
+		$config = new Config([
+								 'foo' => [
+									 'bar' => [
+										 'baz' => 'qux'
+									 ]
+								 ]
+							 ]);
+
+		$this->assertEquals('qux', $config->get('foo.bar.baz'));
+	}
+
+	public function testGetReturnsConfig()
+	{
+		$config = new Config([
+								 'foo' => [
+									 'bar' => [
+										 'baz' => 'qux'
+									 ]
+								 ]
+							 ]);
+
+		$this->assertInstanceOf(Config::class, $config->get('foo.bar'));
+	}
+
+	public function testMultiStepTraversalConfig()
+	{
+		$config = new Config([
+								 'foo' => [
+									 'bar' => [
+										 'baz' => 'qux'
+									 ]
+								 ]
+							 ]);
+
+		$firstTier = $config->get('foo.bar');
+
+		$this->assertEquals('qux', $firstTier->get('baz'));
+	}
+
+	public function testCanTraversal()
+	{
+		$config = new Config([
+								 'foo' => [
+									 'bar' => [
+										 'baz' => 'qux'
+									 ]
+								 ]
+							 ]);
+
+		$this->assertTrue($config->has('foo.bar.baz'));
+	}
+
+	public function testTraversalWithEscapedKeyGet()
+	{
+		$config = new Config([
+								 'foo' => [
+									 'bar.baz' => [
+										 'value' => 'correct',
+									 ],
+									 'bar' => [
+										 'baz' => [
+											 'value' => 'incorrect'
+										 ]
+									 ]
+								 ]
+							 ]);
+
+		$this->assertEquals('correct', $config->get('foo.[bar.baz].value'));
+	}
+
+	public function testTraversalWithIntermediateEscapedKey()
+	{
+		$config = new Config([
+								 'foo' => [
+									 'bar.baz' => [
+										 'value' => 'correct',
+									 ],
+									 'bar' => [
+										 'baz' => [
+											 'value' => 'incorrect'
+										 ]
+									 ]
+								 ]
+							 ]);
+
+		$this->assertEquals(new Config(['value' => 'correct']), $config->get('foo.[bar.baz]'));
+	}
+}
